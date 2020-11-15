e79b69e557a5b9f335f04c22e156cde386d5e14f
FelipeBellinazzi <bellinazzifelipe@gmail.com>
2020-11-14 18:04:10 -0700

    Simple user Array

--- Js/app.js
+++ Js/app.js
@@ -16,6 +16,30 @@ else if (age >=18 && is_subscribed == true) {
     console.log(age + "Older and is subscribed");
 }
 else {
-    console.log(age + "true or false");
+    console.log(age + "Are You a Robot");
+}
+
+
+// W9B
+
+var age = [10, 14, 16, 18, 22];
+var is_subscribed = [true, false,true, false, true];
+
+for (var i = 0; i < age.length; i++) {
+    if (age[i] < 18 && is_subscribed[i] == true) {
+        console.log("You are younger than 18 and subscribed");
+    }
+    else if (age[i] >=18 && is_subscribed[i] == true) {
+        console.log("You are 18 or olde and subscribed");
+    }
+    else if (age[i] >=18 && is_subscribed[i] == false) {
+        console.log("You are 18 or older and not subscribed");
+    }
+    else if (age[i] <18 && is_subscribed[i] == false) {
+        console.log("You are less than 18 and not subscribed")
+    }
+    else {
+        console.log("block")
+    }
 }
 
